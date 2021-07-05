28b5e5ebfce71c821314f65c3c215cb2620e7645
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2021-07-06 01:30:49 +0600

    python3

--- userInput.py
+++ userInput.py
@@ -8,3 +8,12 @@ print("Your ID is: "+ID)
 
 CGPA = input("Enter CGPA: ")
 print("Your CGPA is: "+CGPA)
+
+Address = input("Enter Address: ")  #user input
+print("Your Address is: "+Address)  #output
+
+phone = input("Enter phone: ")
+print("Your phone is: "+phone)
+
+age = input("Enter age: ")
+print("Your age is: "+age)
